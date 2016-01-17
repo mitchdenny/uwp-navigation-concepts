9f4c081a6fdb649972ea47a2ca27d903cce5a77c
Mitch Denny <mitch@mitchdenny.com>
2016-01-17 23:27:04 +1100

    My sandpit.

---
+++ NavAndBind/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavAndBind.ViewModels
+{
+    public class BaseViewModel
+    {
+        public virtual void NavigatingFrom()
+        {
+
+        }
+
+        public virtual void NavigatedTo()
+        {
+
+        }
+
+        public virtual void NavigatedFrom()
+        {
+
+        }
+    }
+}
---
+++ NavAndBind/Views/BaseView.cs
@@ -0,0 +1,45 @@
+﻿using NavAndBind.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace NavAndBind.Views
+{
+    public class BaseView : Page
+    {
+        public BaseViewModel ViewModel
+        {
+            get
+            {
+                return (BaseViewModel)this.DataContext;
+            }
+
+            protected set
+            {
+                this.DataContext = value;
+            }
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            this.ViewModel.NavigatedFrom();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            this.ViewModel.NavigatedTo();
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            this.ViewModel.NavigatingFrom();
+        }
+    }
+}
